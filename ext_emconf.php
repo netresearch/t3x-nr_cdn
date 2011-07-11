d3f3787bf57b4e6de0a0b8cf6766756677dd3134
Andre Hähnel <andre.haehnel@netresearch.de>
2011-07-11 06:59:30 +0000

    SDM-4961 change version
    git-svn-id: svn://svn.nr/www.aida.de/trunk/typo3conf/ext/nr_cdn@12374 2c55e77a-bd54-0410-917c-c657395a2d98

--- ext_emconf.php
+++ ext_emconf.php
@@ -11,37 +11,37 @@
 ########################################################################
 
 $EM_CONF[$_EXTKEY] = array(
-	'title' => 'NR CDN',
-	'description' => 'Images and Medias to Content Delivery Network',
-	'category' => 'fe',
-	'author' => 'Alexander Opitz',
-	'author_company' => 'Netresearch GmbH & Co.KG',
-	'author_email' => 'alexander.opitz@netresearch.de',
-	'shy' => '',
-	'dependencies' => '',
-	'constraints' => array(
-		'depends' => array(
-			'typo3' => '4.2.1-0.0.0',
-		),
-		'conflicts' => array(
-		),
-		'suggests' => array(
-		),
-	),
-	'conflicts' => '',
-	'priority' => '',
-	'module' => '',
-	'state' => 'beta',
-	'internal' => '',
-	'uploadfolder' => 0,
-	'createDirs' => '',
-	'modify_tables' => '',
-	'clearCacheOnLoad' => 0,
-	'lockType' => '',
-	'version' => '0.9.4',
-	'_md5_values_when_last_written' => 'a:7:{s:9:"Changelog";s:4:"7a63";s:10:"README.txt";s:4:"4ae8";s:35:"class.user_cssstyledcontent_pi1.php";s:4:"bd1d";s:26:"class.ux_tslib_content.php";s:4:"38f2";s:21:"class.ux_tslib_fe.php";s:4:"0c91";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"d777";}',
-	'suggests' => array(
-	),
+    'title' => 'NR CDN',
+    'description' => 'Images and Medias to Content Delivery Network',
+    'category' => 'fe',
+    'author' => 'Alexander Opitz',
+    'author_company' => 'Netresearch GmbH & Co.KG',
+    'author_email' => 'alexander.opitz@netresearch.de',
+    'shy' => '',
+    'dependencies' => '',
+    'constraints' => array(
+        'depends' => array(
+            'typo3' => '4.2.1-0.0.0',
+        ),
+        'conflicts' => array(
+        ),
+        'suggests' => array(
+        ),
+    ),
+    'conflicts' => '',
+    'priority' => '',
+    'module' => '',
+    'state' => 'beta',
+    'internal' => '',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'modify_tables' => '',
+    'clearCacheOnLoad' => 0,
+    'lockType' => '',
+    'version' => '0.9.5',
+    '_md5_values_when_last_written' => 'a:7:{s:9:"Changelog";s:4:"7a63";s:10:"README.txt";s:4:"4ae8";s:35:"class.user_cssstyledcontent_pi1.php";s:4:"bd1d";s:26:"class.ux_tslib_content.php";s:4:"38f2";s:21:"class.ux_tslib_fe.php";s:4:"0c91";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"d777";}',
+    'suggests' => array(
+    ),
 );
 
 ?>
