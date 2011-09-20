aa3785fd767c8dcd47b141b29cfb13c1ed7e0aa9
Sebastian Mendel <sebastian.mendel@netresearch.de>
2011-09-20 18:19:29 +0200

    CS

--- class.user_cssstyledcontent_pi1.php
+++ class.user_cssstyledcontent_pi1.php
@@ -1,24 +1,44 @@
 <?php
+declare(encoding = 'UTF-8');
+/**
+ * Extension config script
+ *
+ * PHP version 5
+ *
+ * @category   Netresearch
+ * @package    CDN
+ * @subpackage Controller
+ * @author     Sebastian Mendel <sebastian.mendel@netresearch.de>
+ * @license    http://www.netresearch.de Netresearch
+ * @link       http://www.netresearch.de
+ */
+
 
 class user_cssstyledcontent_pi1
 {
-	/**
-	 * return url from file
-	 *
-	 * @param	string		$url: file url
-	 * @param	array		$conf typoscript configuration
+    /**
+     * return url from file
      *
-	 * @return	string		url
-	 */
-	function getFileUrl($url, $conf)
+     * @param string $url  file url
+     * @param array  $conf typoscript configuration
+     *
+     * @return string url
+     */
+    function getFileUrl($url, $conf)
     {
         $arConfig = $GLOBALS['TSFE']->tmpl->setup['config.']['nr_cdn.'];
         $output = '';
-        $initP = '?id='.$GLOBALS['TSFE']->id.'&type='.$GLOBALS['TSFE']->type;
-        if (@is_file($url))	{
+        $initP  = '?id=' . $GLOBALS['TSFE']->id . '&type=' . $GLOBALS['TSFE']->type;
+        if (@is_file($url)) {
             $urlEnc = str_replace('%2F', '/', rawurlencode($url));
-            $locDataAdd = $conf['jumpurl.']['secure'] ? $this->cObj->locDataJU($urlEnc,$conf['jumpurl.']['secure.']) : '';
-            $retUrl = ($conf['jumpurl']) ? $GLOBALS['TSFE']->config['mainScript'].$initP.'&jumpurl='.rawurlencode($urlEnc).$locDataAdd.$GLOBALS['TSFE']->getMethodUrlIdToken : $urlEnc;		// && $GLOBALS['TSFE']->config['config']['jumpurl_enable']
+            $locDataAdd = $conf['jumpurl.']['secure'] 
+                ? $this->cObj->locDataJU($urlEnc, $conf['jumpurl.']['secure.']) 
+                : '';
+            $retUrl = ($conf['jumpurl']) 
+                ? $GLOBALS['TSFE']->config['mainScript'] . $initP . '&jumpurl='
+                    . rawurlencode($urlEnc)
+                    . $locDataAdd . $GLOBALS['TSFE']->getMethodUrlIdToken 
+                : $urlEnc;// && $GLOBALS['TSFE']->config['config']['jumpurl_enable']
             if (is_array($arConfig) && isset($arConfig['URL'])) {
                 $retUrl = str_replace(
                     'fileadmin/',
@@ -26,15 +46,16 @@ class user_cssstyledcontent_pi1
                     $retUrl
                 );
             }
-            return htmlspecialchars($GLOBALS['TSFE']->absRefPrefix.$retUrl);
+            return htmlspecialchars($GLOBALS['TSFE']->absRefPrefix . $retUrl);
         };
+        
         return '';
     }
 }
 
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_cdn/class.user_cssstyledcontent_pi1.php'])	{
-	include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_cdn/class.user_cssstyledcontent_pi1.php']);
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_cdn/class.user_cssstyledcontent_pi1.php'])    {
+    include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_cdn/class.user_cssstyledcontent_pi1.php']);
 }
 
 ?>
--- class.ux_tslib_content.php
+++ class.ux_tslib_content.php
@@ -1,26 +1,46 @@
 <?php
+declare(encoding = 'UTF-8');
+/**
+ * Extension config script
+ *
+ * PHP version 5
+ *
+ * @category   Netresearch
+ * @package    CDN
+ * @subpackage Controller
+ * @author     Sebastian Mendel <sebastian.mendel@netresearch.de>
+ * @license    http://www.netresearch.de Netresearch
+ * @link       http://www.netresearch.de
+ */
 
-  // Includes this classes since it is used for parsing HTML
-require_once(PATH_t3lib."class.t3lib_parsehtml.php");
+/**
+ * Includes this classes since it is used for parsing HTML
+ */
+require_once PATH_t3lib . 'class.t3lib_parsehtml.php';
 
-    // Object TypoScript library included:
-if(t3lib_extMgm::isLoaded('obts')) {
-    require_once(t3lib_extMgm::extPath('obts').'_tsobject/_tso.php');
+/**
+ * Object TypoScript library included:
+ */
+if (t3lib_extMgm::isLoaded('obts')) {
+    require_once t3lib_extMgm::extPath('obts', '_tsobject/_tso.php');
 }
 
 class ux_tslib_cObj extends tslib_cObj
 {
     /**
-    * Rendering the cObject, TEXT
-    *
-    * @param	array		Array of TypoScript properties
-    * @return	string		Output
-    * @link http://typo3.org/doc.0.html?&tx_extrepmgm_pi1[extUid]=270&tx_extrepmgm_pi1[tocEl]=350&cHash=b49de28f83
-    */
+     * Rendering the cObject, TEXT
+     *
+     * @param array $conf TypoScript properties
+     * 
+     * @return string Output
+     * @link http://typo3.org/doc.0.html?&tx_extrepmgm_pi1[extUid]=270&tx_extrepmgm_pi1[tocEl]=350&cHash=b49de28f83
+     */
     function TEXT($conf)
     {
         $arConfig = $GLOBALS['TSFE']->tmpl->setup['config.']['nr_cdn.'];
-        $content = $this->stdWrap($conf['value'],$conf);
+        
+        $content = $this->stdWrap($conf['value'], $conf);
+        
         if (is_array($arConfig) && ! empty($arConfig['URL'])) {
             $content = str_replace(
                 '"fileadmin/',
@@ -32,15 +52,17 @@ class ux_tslib_cObj extends tslib_cObj
     }
 
 
+    
     /**
-    * Rendering the cObject, USER and USER_INT
-    *
-    * @param	array		Array of TypoScript properties
-    * @param	string		If "INT" then the cObject is a "USER_INT" (non-cached), otherwise just "USER" (cached)
-    * @return	string		Output
-    * @link http://typo3.org/doc.0.html?&tx_extrepmgm_pi1[extUid]=270&tx_extrepmgm_pi1[tocEl]=369&cHash=b623aca0a9
-    */
-    function USER($conf,$ext='')
+     * Rendering the cObject, USER and USER_INT
+     *
+     * @param array  $conf TypoScript properties
+     * @param string $ext  If "INT" then the cObject is a "USER_INT" (non-cached), otherwise just "USER" (cached)
+     * 
+     * @return string Output
+     * @link http://typo3.org/doc.0.html?&tx_extrepmgm_pi1[extUid]=270&tx_extrepmgm_pi1[tocEl]=369&cHash=b623aca0a9
+     */
+    function USER($conf, $ext = '')
     {
         $content = parent::USER($conf, $ext);
         $arConfig = $GLOBALS['TSFE']->tmpl->setup['config.']['nr_cdn.'];
@@ -54,19 +76,24 @@ class ux_tslib_cObj extends tslib_cObj
         return $content;
     }
 
+    
 
     /**
-    * Rendering the cObject, MULTIMEDIA
-    *
-    * @param	array		Array of TypoScript properties
-    * @return	string		Output
-    * @link http://typo3.org/doc.0.html?&tx_extrepmgm_pi1[extUid]=270&tx_extrepmgm_pi1[tocEl]=374&cHash=efd88ab4a9
-    */
-    function MULTIMEDIA($conf)	{
+     * Rendering the cObject, MULTIMEDIA
+     *
+     * @param array $conf TypoScript properties
+     * 
+     * @return string Output
+     * @link http://typo3.org/doc.0.html?&tx_extrepmgm_pi1[extUid]=270&tx_extrepmgm_pi1[tocEl]=374&cHash=efd88ab4a9
+     */
+    function MULTIMEDIA($conf)    
+    {
         $arConfig = $GLOBALS['TSFE']->tmpl->setup['config.']['nr_cdn.'];
-        $tmpAbsPrefix = $GLOBALS['TSFE']->absRefPrefix;
+        
+        $tmpAbsPrefix   = $GLOBALS['TSFE']->absRefPrefix;
         $conf['width']  = 200;
         $conf['height'] = 200;
+        
         if (is_array($arConfig) && isset($arConfig['URL'])) {
             $GLOBALS['TSFE']->absRefPrefix = $arConfig['URL'] . '/';
         }
@@ -74,25 +101,27 @@ class ux_tslib_cObj extends tslib_cObj
         $content = parent::MULTIMEDIA($conf);
         $GLOBALS['TSFE']->absRefPrefix = $tmpAbsPrefix;
         return $content;
-
     }
 
 
     /**
-    * Returns a <img> tag with the image file defined by $file and processed according to the properties in the TypoScript array.
-    * Mostly this function is a sub-function to the IMAGE function which renders the IMAGE cObject in TypoScript. This function is called by "$this->cImage($conf['file'],$conf);" from IMAGE().
-    *
-    * @param	string		File TypoScript resource
-    * @param	array		TypoScript configuration properties
-    * @return	string		<img> tag, (possibly wrapped in links and other HTML) if any image found.
-    * @access private
-    * @see IMAGE()
-    */
-    function cImage($file,$conf) {
-
-        $info = $this->getImgResource($file,$conf['file.']);
+     * Returns a <img> tag with the image file defined by $file and processed according to the properties in the TypoScript array.
+     * Mostly this function is a sub-function to the IMAGE function which renders the IMAGE cObject in TypoScript. This function is called by "$this->cImage($conf['file'],$conf);" from IMAGE().
+     *
+     * @param string $file File TypoScript resource
+     * @param array  $conf TypoScript configuration properties
+     * 
+     * @return string <img> tag, (possibly wrapped in links and other HTML) if any image found.
+     * @access private
+     * @see IMAGE()
+     */
+    function cImage($file, $conf) 
+    {
+        $info = $this->getImgResource($file, $conf['file.']);
+        
         if (is_array($info)) {
             $arConfig = $GLOBALS['TSFE']->tmpl->setup['config.']['nr_cdn.'];
+            
             $tmpAbsPrefix = $GLOBALS['TSFE']->absRefPrefix;
             if (is_array($arConfig) && ! empty($arConfig['URL'])
                 && strncmp('fileadmin/', $info[3], 10) === 0
@@ -104,11 +133,9 @@ class ux_tslib_cObj extends tslib_cObj
             return $content;
         }
     }
-
-
 }
 
-if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_cdn/class.ux_tslib_content.php'])	{
+if (defined('TYPO3_MODE') && $TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_cdn/class.ux_tslib_content.php'])    {
     include_once($TYPO3_CONF_VARS[TYPO3_MODE]['XCLASS']['ext/nr_cdn/class.ux_tslib_content.php']);
 }
 ?>
