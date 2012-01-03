67256106dba539fafc0faa0d3d0f1c41f8bea1a6
Alexander Opitz <alexander.opitz@netresearch.de>
2012-01-03 12:16:59 +0100

    Version: 0.9.7

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "nr_cdn".
 #
-# Auto generated 20-12-2011 14:14
+# Auto generated 03-01-2012 12:16
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -39,8 +39,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.9.6',
-	'_md5_values_when_last_written' => 'a:35:{s:9:"Changelog";s:4:"7e82";s:10:"README.txt";s:4:"4ae8";s:9:"build.xml";s:4:"0b0a";s:35:"class.user_cssstyledcontent_pi1.php";s:4:"e423";s:32:"class.user_t3libpagerenderer.php";s:4:"1a0f";s:26:"class.ux_tslib_content.php";s:4:"220c";s:21:"class.ux_tslib_fe.php";s:4:"0d2a";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"dfac";s:26:"tests/PageRendererTest.php";s:4:"9b49";s:23:"tests/UxContentTest.php";s:4:"e850";s:19:"tests/bootstrap.php";s:4:"d2d3";s:20:"tests/dummy-cObj.php";s:4:"e02d";s:17:"tests/phpunit.xml";s:4:"da54";s:59:"tests/doc/coverage/class.user_cssstyledcontent_pi1.php.html";s:4:"559a";s:56:"tests/doc/coverage/class.user_t3libpagerenderer.php.html";s:4:"9ecd";s:50:"tests/doc/coverage/class.ux_tslib_content.php.html";s:4:"e888";s:45:"tests/doc/coverage/class.ux_tslib_fe.php.html";s:4:"cba8";s:32:"tests/doc/coverage/close12_1.gif";s:4:"770d";s:35:"tests/doc/coverage/container-min.js";s:4:"5511";s:32:"tests/doc/coverage/container.css";s:4:"495d";s:32:"tests/doc/coverage/directory.png";s:4:"0c2e";s:38:"tests/doc/coverage/ext_emconf.php.html";s:4:"bf44";s:41:"tests/doc/coverage/ext_localconf.php.html";s:4:"718f";s:27:"tests/doc/coverage/file.png";s:4:"6983";s:28:"tests/doc/coverage/glass.png";s:4:"d0bc";s:32:"tests/doc/coverage/highcharts.js";s:4:"08cd";s:39:"tests/doc/coverage/index.dashboard.html";s:4:"441a";s:29:"tests/doc/coverage/index.html";s:4:"c163";s:32:"tests/doc/coverage/jquery.min.js";s:4:"0efa";s:28:"tests/doc/coverage/style.css";s:4:"6720";s:37:"tests/doc/coverage/yahoo-dom-event.js";s:4:"366e";s:21:"tests/logs/clover.xml";s:4:"b8db";s:19:"tests/logs/dox.html";s:4:"b31c";s:20:"tests/logs/junit.xml";s:4:"e0ce";}',
+	'version' => '0.9.7',
+	'_md5_values_when_last_written' => 'a:14:{s:9:"ChangeLog";s:4:"2a1f";s:10:"README.txt";s:4:"4ae8";s:9:"build.xml";s:4:"0b0a";s:35:"class.user_cssstyledcontent_pi1.php";s:4:"e423";s:32:"class.user_t3libpagerenderer.php";s:4:"6905";s:26:"class.ux_tslib_content.php";s:4:"220c";s:21:"class.ux_tslib_fe.php";s:4:"0d2a";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"dfac";s:26:"tests/PageRendererTest.php";s:4:"0f78";s:23:"tests/UxContentTest.php";s:4:"e850";s:19:"tests/bootstrap.php";s:4:"d2d3";s:20:"tests/dummy-cObj.php";s:4:"e02d";s:17:"tests/phpunit.xml";s:4:"da54";}',
 	'suggests' => array(
 	),
 );
