39bf759d9a6a6a34e4e0b715a3bc74f20e5dc199
Sebastian Mendel <sebastian.mendel@netresearch.de>
2012-04-26 17:05:40 +0200

    "Version 0.11.0"

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 ########################################################################
 # Extension Manager/Repository config file for ext "nr_cdn".
 #
-# Auto generated 01-02-2012 14:11
+# Auto generated 26-04-2012 17:05
 #
 # Manual updates:
 # Only the data in the array - everything else is removed by next
@@ -40,8 +40,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '0.10.0',
-	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"6d6f";s:10:"README.txt";s:4:"66e4";s:9:"build.xml";s:4:"c0f9";s:32:"class.user_t3libpagerenderer.php";s:4:"cbed";s:26:"class.ux_tslib_content.php";s:4:"7a87";s:21:"class.ux_tslib_fe.php";s:4:"640a";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"650e";s:23:"src/Netresearch/Cdn.php";s:4:"0b50";s:50:"src/Netresearch/Cdn/HookCssFilelinksGetFileUrl.php";s:4:"740d";s:26:"tests/PageRendererTest.php";s:4:"0003";s:23:"tests/UxContentTest.php";s:4:"4b92";s:19:"tests/bootstrap.php";s:4:"d2d3";s:20:"tests/dummy-cObj.php";s:4:"e02d";s:17:"tests/phpunit.xml";s:4:"da54";}',
+	'version' => '0.11.0',
+	'_md5_values_when_last_written' => 'a:16:{s:9:"ChangeLog";s:4:"db1e";s:10:"README.rst";s:4:"70c5";s:9:"build.xml";s:4:"c0f9";s:32:"class.user_t3libpagerenderer.php";s:4:"a363";s:26:"class.ux_tslib_content.php";s:4:"7a87";s:21:"class.ux_tslib_fe.php";s:4:"8d54";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"650e";s:23:"src/Netresearch/Cdn.php";s:4:"c955";s:50:"src/Netresearch/Cdn/HookCssFilelinksGetFileUrl.php";s:4:"740d";s:26:"tests/PageRendererTest.php";s:4:"cb5e";s:23:"tests/UxContentTest.php";s:4:"4b92";s:19:"tests/bootstrap.php";s:4:"d2d3";s:20:"tests/dummy-cObj.php";s:4:"e02d";s:17:"tests/phpunit.xml";s:4:"da54";s:29:"tests/Netresearch/CdnTest.php";s:4:"afdf";}',
 	'suggests' => array(
 	),
 );
