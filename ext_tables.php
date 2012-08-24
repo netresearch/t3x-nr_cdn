4a80d430f6925aa2795112f74155779f21219c2c
Sebastian Mendel <sebastian.mendel@netresearch.de>
2012-08-24 12:58:59 +0200

    add TypoScript constant and setup file

---
+++ ext_tables.php
@@ -0,0 +1,23 @@
+<?php
+declare(encoding = 'UTF-8');
+/**
+ * Extension config script
+ *
+ * PHP version 5
+ *
+ * @category   Netresearch
+ * @package    CDN
+ * @subpackage Config
+ * @author     Sebastian Mendel <sebastian.mendel@netresearch.de>
+ * @license    http://www.netresearch.de Netresearch
+ * @link       http://www.netresearch.de
+ */
+
+/**
+ *
+ */
+defined('TYPO3_MODE') or die('Access denied.');
+
+t3lib_extMgm::addStaticFile(
+    $_EXTKEY, "static", "NR CDN Setup"
+);
