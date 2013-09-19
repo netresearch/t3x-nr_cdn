99e0165b4ca1d7bf5b0a71c6fd2cb3e938264fa8
Tobias Hein <tobias.hein@netresearch.de>
2013-09-19 14:42:03 +0200

    Version: 1.1.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,7 +3,7 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "nr_cdn".
  *
- * Auto generated 07-03-2013 09:41
+ * Auto generated 19-09-2013 14:42
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
@@ -38,8 +38,8 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.0.1',
-	'_md5_values_when_last_written' => 'a:20:{s:9:"build.xml";s:4:"c0f9";s:9:"ChangeLog";s:4:"a59e";s:32:"class.user_t3libpagerenderer.php";s:4:"5949";s:26:"class.ux_tslib_content.php";s:4:"bdc6";s:21:"class.ux_tslib_fe.php";s:4:"9e7f";s:16:"ext_autoload.php";s:4:"696f";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"40e5";s:14:"ext_tables.php";s:4:"ce5f";s:10:"README.rst";s:4:"0bbf";s:23:"src/Netresearch/Cdn.php";s:4:"45be";s:50:"src/Netresearch/Cdn/HookCssFilelinksGetFileUrl.php";s:4:"dd33";s:20:"static/constants.txt";s:4:"9c4c";s:16:"static/setup.txt";s:4:"bf36";s:19:"tests/bootstrap.php";s:4:"d2d3";s:20:"tests/dummy-cObj.php";s:4:"da35";s:26:"tests/PageRendererTest.php";s:4:"6b85";s:17:"tests/phpunit.xml";s:4:"c1d6";s:23:"tests/UxContentTest.php";s:4:"414a";s:29:"tests/Netresearch/CdnTest.php";s:4:"5492";}',
+	'version' => '1.1.0',
+	'_md5_values_when_last_written' => 'a:45:{s:9:"build.xml";s:4:"687b";s:9:"ChangeLog";s:4:"0684";s:32:"class.user_t3libpagerenderer.php";s:4:"3b4d";s:26:"class.ux_tslib_content.php";s:4:"04ce";s:21:"class.ux_tslib_fe.php";s:4:"c641";s:16:"ext_autoload.php";s:4:"6f54";s:12:"ext_icon.gif";s:4:"a459";s:17:"ext_localconf.php";s:4:"de58";s:14:"ext_tables.php";s:4:"185b";s:10:"README.rst";s:4:"2210";s:23:"src/Netresearch/Cdn.php";s:4:"014f";s:50:"src/Netresearch/Cdn/HookCssFilelinksGetFileUrl.php";s:4:"624d";s:20:"static/constants.txt";s:4:"1279";s:16:"static/setup.txt";s:4:"771b";s:19:"tests/bootstrap.php";s:4:"af3c";s:20:"tests/dummy-cObj.php";s:4:"6d5b";s:26:"tests/PageRendererTest.php";s:4:"a0c5";s:17:"tests/phpunit.xml";s:4:"439e";s:23:"tests/UxContentTest.php";s:4:"7c16";s:29:"tests/Netresearch/CdnTest.php";s:4:"0cc8";s:56:"tests/doc/coverage/class.user_t3libpagerenderer.php.html";s:4:"b840";s:50:"tests/doc/coverage/class.ux_tslib_content.php.html";s:4:"239f";s:45:"tests/doc/coverage/class.ux_tslib_fe.php.html";s:4:"9bdb";s:39:"tests/doc/coverage/index.dashboard.html";s:4:"3a1b";s:29:"tests/doc/coverage/index.html";s:4:"c5af";s:37:"tests/doc/coverage/src.dashboard.html";s:4:"831c";s:27:"tests/doc/coverage/src.html";s:4:"39c8";s:49:"tests/doc/coverage/src_Netresearch.dashboard.html";s:4:"6f8b";s:39:"tests/doc/coverage/src_Netresearch.html";s:4:"44dd";s:53:"tests/doc/coverage/src_Netresearch_Cdn.dashboard.html";s:4:"92b9";s:43:"tests/doc/coverage/src_Netresearch_Cdn.html";s:4:"fd23";s:47:"tests/doc/coverage/src_Netresearch_Cdn.php.html";s:4:"ba00";s:74:"tests/doc/coverage/src_Netresearch_Cdn_HookCssFilelinksGetFileUrl.php.html";s:4:"8b9c";s:51:"tests/doc/coverage/css/bootstrap-responsive.min.css";s:4:"f800";s:40:"tests/doc/coverage/css/bootstrap.min.css";s:4:"2d0e";s:32:"tests/doc/coverage/css/style.css";s:4:"b0a6";s:53:"tests/doc/coverage/img/glyphicons-halflings-white.png";s:4:"9bbc";s:47:"tests/doc/coverage/img/glyphicons-halflings.png";s:4:"2516";s:38:"tests/doc/coverage/js/bootstrap.min.js";s:4:"9e8a";s:35:"tests/doc/coverage/js/highcharts.js";s:4:"8b61";s:34:"tests/doc/coverage/js/html5shiv.js";s:4:"262b";s:35:"tests/doc/coverage/js/jquery.min.js";s:4:"3977";s:21:"tests/logs/clover.xml";s:4:"2c3c";s:19:"tests/logs/dox.html";s:4:"3169";s:20:"tests/logs/junit.xml";s:4:"5f7f";}',
 	'suggests' => array(
 	),
 );
